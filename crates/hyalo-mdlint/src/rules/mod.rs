7716bcb56c16532c94279733fc5a6af521ce4631
Jean-Pierre Bergamin <james@ractive.ch>
2026-09-05 12:49:17 +0200

    wip(iter-271): BodySpans + MD031 unterminated-fence and code-block filters, markdownlint-disable comments

--- crates/hyalo-mdlint/src/rules/mod.rs
+++ crates/hyalo-mdlint/src/rules/mod.rs
@@ -7,6 +7,7 @@ pub mod hyalo003;
 pub mod hyalo004;
 pub mod hyalo007;
 pub mod obsidian;
+pub mod spans;
 
 pub use hyalo001::Hyalo001;
 pub use hyalo002::Hyalo002;
@@ -16,3 +17,4 @@ pub use hyalo007::non_scalar_title_kind;
 pub use obsidian::{
     is_obsidian_tag_line, is_obsidian_tag_token, link_text_is_image, url_is_inside_link_markup,
 };
+pub use spans::BodySpans;
